ab33922a48e7e74860a540674d391bfdedcd43d3
Melderan <Melderan@gmail.com>
2014-01-09 07:03:52 -0600

    Add API auth for each challenge

--- challenge2.php
+++ challenge2.php
@@ -1,1 +1,12 @@
-require 'vendor/autoload.php';
+<?php
+    // require libraries
+    require 'vendor/autoload.php';
+ 
+    // Namespaces
+    use OpenCloud\Rackspace;
+    use OpenCloud\Compute\Constants\Network;
+    use OpenCloud\Compute\Constants\ServerState;
+
+    $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, parse_ini_file('api-credentials.ini', true)['Rax-Personal']);
+
+    
--- challenge4.php
+++ challenge4.php
@@ -1,1 +1,10 @@
-require 'vendor/autoload.php';
+<?php
+    // require libraries
+    require 'vendor/autoload.php';
+ 
+    // Namespaces
+    use OpenCloud\Rackspace;
+
+    $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, parse_ini_file('api-credentials.ini', true)['Rax-Personal']);
+
+    
